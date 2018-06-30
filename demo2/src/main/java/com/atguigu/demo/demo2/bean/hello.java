f1ad28f60d7c3f1cd9ef7e4024cca0b9974ecc4f
feifei123m <40225451+feifei123m@users.noreply.github.com>
2018-06-30 18:10:53 +0800

    Git 端更新
    I‘m ok .....’

--- demo2/src/main/java/com/atguigu/demo/demo2/bean/hello.java
+++ demo2/src/main/java/com/atguigu/demo/demo2/bean/hello.java
@@ -3,6 +3,7 @@ package com.atguigu.demo.demo2.bean;
 public class hello {
     public static void main(String[] args){
         System.out.println("hello, test git");
+        System.out.println("I'm  ok....");
     }
 
 }
