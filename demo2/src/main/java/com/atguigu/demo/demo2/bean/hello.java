e49d5a2e4f9ab9a5e17f25daf914ddd6c7ab0133
常飞 <admin@atguigu.com>
2018-06-30 18:14:01 +0800

    最新回应，收到请回复...

--- demo2/src/main/java/com/atguigu/demo/demo2/bean/hello.java
+++ demo2/src/main/java/com/atguigu/demo/demo2/bean/hello.java
@@ -4,6 +4,7 @@ public class hello {
     public static void main(String[] args){
         System.out.println("hello, test git");
         System.out.println("I'm  ok....");
+        System.out.println("收到，了解....");
     }
 
 }
