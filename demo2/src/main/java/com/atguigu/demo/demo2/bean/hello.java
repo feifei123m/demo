7fee70faff899928b2b6470eb4e79d616dfde099
常飞 <admin@atguigu.com>
2018-08-04 18:02:05 +0800

    联系了一下小辣椒，还可以

--- demo2/src/main/java/com/atguigu/demo/demo2/bean/hello.java
+++ demo2/src/main/java/com/atguigu/demo/demo2/bean/hello.java
@@ -1,7 +1,13 @@
 package com.atguigu.demo.demo2.bean;
 
 public class hello {
+
     public static void main(String[] args){
+
+        person person = new person();
+        person.setId(2).setName("z2");
+        System.out.println(person.toString());
+
         System.out.println("hello, test git");
         System.out.println("I'm  ok....");
         System.out.println("收到，了解....");
